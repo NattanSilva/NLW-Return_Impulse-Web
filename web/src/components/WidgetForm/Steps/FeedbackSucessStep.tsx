31d3d464cc2932f18bc25d75822b6028663c7cd1
natanael leite silva <natanxboy@gmail.com>
2022-05-06 11:05:52 -0300

    Feedback Widget

---
+++ web/src/components/WidgetForm/Steps/FeedbackSucessStep.tsx
@@ -0,0 +1,30 @@
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackSucessStepProps {
+  onFeedbackRestartRequested: () =>void;
+}
+
+export function FeedbackSucessStep({ 
+  onFeedbackRestartRequested 
+}: FeedbackSucessStepProps) {
+  return(
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src="src/assets/sucess.svg" alt="" />
+
+        <span className="text-xl mt-2">Agradecemos seu feedback!</span>
+
+        <button
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+          onClick={ onFeedbackRestartRequested }
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
+  )
+}
---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
+
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de uma nuvem de pensamento'
+    }
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function Widgetform() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType  | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return(
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      { feedbackSent ? (
+        <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType} 
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+
+      <footer className="text-xs text-neutral-400">
+        Feito com amor pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Rocketseat</a>
+      </footer>
+    </div>
+  );
+}
