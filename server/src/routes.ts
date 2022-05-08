48d571f5382cb2449ca998ea253d8560906f408f
natanael leite silva <natanxboy@gmail.com>
2022-05-08 02:08:55 -0300

    COMPLETO

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,7 +7,7 @@ export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
-  
+
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
   const nodemailerMailAdapter = new NodemailerMailAdapter()
 
@@ -18,10 +18,11 @@ routes.post('/feedbacks', async (req, res) => {
 
   await submitFeedbackUseCase.execute({
     type,
-    comment,
+   comment,
     screenshot,
   })
 
   return res.status(201).send();
+  
 
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,10 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server running!');
 });
